bb37d0d4e9e65226ca1b507d4f86bbe2692423a8
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2017-12-06 23:35:07 +0700

    Initial Commit

---
+++ app/countdown.js
@@ -0,0 +1,20 @@
+const padTime = val => _.padStart(val, 2, '0');
+const padMilliSec = val => _.padStart(val, 3, '0');
+
+const timeSelector = document.getElementById('time');
+
+const interval = setInterval(() => {
+  const deadline = moment('7-12-2017', 'D-MM-YYYY');
+  const interval = moment(deadline).diff(moment()); // Millisecond unit
+  if (interval > 0) {
+    const ms = interval % 1000;
+    const second = Math.floor((interval / 1000) % 60).toFixed(0);
+    const min = Math.floor((interval / 60000) % 60).toFixed(0);
+    const hour = Math.floor(((interval / (3600000)) % 24)).toFixed(0);
+    const display = `${padTime(hour)}:${padTime(min)}:${padTime(second)}.${padMilliSec(ms)}`;
+    console.log(display);
+    timeSelector.innerHTML = display;
+  } else {
+    clearInterval(interval);
+  }
+}, 1);
