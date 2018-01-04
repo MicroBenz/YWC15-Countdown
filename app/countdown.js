4ef1ceba36c399cca0fb4d21f7ec56bb1212f301
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2018-01-04 13:29:31 +0700

    Handle fake deadline

--- app/countdown.js
+++ app/countdown.js
@@ -1,1 +1,33 @@
-const padTime=a=>_.padStart(a,2,'0'),padMilliSec=a=>_.padStart(a,3,'0'),timeSelector=document.getElementById('time'),interval=setInterval(()=>{const a=moment('4-1-2018','D-MM-YYYY'),b=moment(a).diff(moment());if(0<b){const d=Math.floor(b/1e3%60).toFixed(0),e=Math.floor(b/6e4%60).toFixed(0),f=Math.floor(b/3.6e6%24).toFixed(0),g=`${padTime(f)}:${padTime(e)}:${padTime(d)}.${padMilliSec(b%1e3)}`;timeSelector.innerHTML=g}else clearInterval(b)},1);
+const padTime = val => _.padStart(val, 2, '0');
+const padMilliSec = val => _.padStart(val, 3, '0');
+
+const timeSelector = document.getElementById('time');
+
+const convertIntervalToTime = interval => ({
+  second: Math.floor((interval / 1000) % 60).toFixed(0),
+  min: Math.floor((interval / 60000) % 60).toFixed(0),
+  hour: Math.floor(((interval / (3600000)) % 24)).toFixed(0)
+});
+
+const fakeDeadline = moment('6-1-2018 17:50.00', 'D-MM-YYYY HH:mm.ss');
+const trueDeadline = moment('6-1-2018 18:00.00', 'D-MM-YYYY HH:mm.ss');
+
+const useFakeDeadline = moment().isBefore(fakeDeadline);
+
+const interval = setInterval(() => {
+  let msInterval;
+  if (useFakeDeadline) {
+    msInterval = moment(fakeDeadline).diff(moment());
+  } else {
+    msInterval = moment(trueDeadline).diff(moment());
+  }
+  if (msInterval > 0) {
+    const ms = msInterval % 1000;
+    const { second, min, hour } = convertIntervalToTime(msInterval);
+    const display = `${padTime(hour)}:${padTime(min)}:${padTime(second)}.${padMilliSec(ms)}`;
+    timeSelector.innerHTML = display;
+  } else {
+    timeSelector.innerHTML = '00:00:00.000';
+    clearInterval(interval);
+  }
+}, 1);
